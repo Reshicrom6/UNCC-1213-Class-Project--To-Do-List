6cfa9433eb81250a80bb37a3adb79b595564b3ac
Reshicrom6 <dunkanjd@gmail.com>
2025-09-23 15:57:06 -0400

    testing separate object files for category and time for simplification purposes

---
+++ src/Time.java
@@ -0,0 +1,11 @@
+/**
+ * TODO Write a one-sentence summary of your class here.
+ * TODO Follow it with additional details about its purpose, what abstraction
+ * it represents, and how to use it.
+ *
+ * @author  TODO Your Name
+ * @version Sep 23, 2025
+ */
+public class Time {
+    
+}
